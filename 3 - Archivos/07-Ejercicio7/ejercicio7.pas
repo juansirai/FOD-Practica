fab1d9bd5008492eea122ed228e02c315e1884c6
juans <juanbsirai664@gmail.com>
2022-04-21 21:29:25 -0300

    ejercicio 7 completo

--- 3 - Archivos/07-Ejercicio7/ejercicio7.pas
+++ 3 - Archivos/07-Ejercicio7/ejercicio7.pas
@@ -14,7 +14,7 @@ Nota: Las bajas deben finalizar al recibir el código 500000
 }
 
 
-program untitled;
+program ejercicio7;
 
 CONST 
 	CORTE = 500000;
@@ -32,7 +32,7 @@ TYPE
 	maestro = file of ave;
 		
 {carga un archivo binario a partir de un txt}
-procedure generarMaestro(var arch: maestro; texto:text);
+procedure generarMaestro(var arch: maestro; var texto:text);
 var
 	dato : ave;
 begin
@@ -70,15 +70,65 @@ begin
 end;
 
 
+
+{marca el registro}
+procedure marcarRegistro(var arch:maestro; cod_eliminar:integer);
+var
+	dato:ave;
+begin
+	reset(arch);                                                              {como no está ordenado, me conviene resetear cada vez que busco}
+	read(arch, dato);  	                                                {asumo que el codigo va a estar en el archivo}
+	while (dato.codigo <> cod_eliminar) do
+		read(arch, dato);
+		
+	dato.nombre:= 'ZZZ';                                             {como asumo que existe, si salgo es porque encontré}
+	seek(arch, filePos(arch)-1);
+	write(arch, dato);
+	close(arch);
+end;
+
 {realiza la baja logica de cada ave pasada por teclado}
 procedure bajaLogica(var arch:maestro);
 var
-	dato: ave;
 	cod_eliminar: longInt;
 begin
 	writeln('Bienvenido al modulo de bajas');
 	write('Ingrese el codigo del ave a dar de baja, ingrese 500000 para finalizar: ');
 	readln(cod_eliminar);
+	
+	while cod_eliminar<> CORTE do begin
+		marcarRegistro(arch, cod_eliminar);
+		write('Ingrese el codigo del ave a dar de baja, ingrese 500000 para finalizar: ');
+		readln(cod_eliminar);
+	end;
+end;
+
+
+{compacta el archivo luego de una baja logica}
+procedure compactarMaestro(var arch:maestro);
+var
+	dato_baja:ave;
+	dato_reemplazo: ave;
+	pos:integer;
+begin
+	reset(arch);
+	
+	while not EOF(arch) do begin
+		read(arch,dato_baja);
+		if(dato_baja.nombre='ZZZ') then begin
+			pos:= filepos(arch)-1;                                                        {guardo la posicion del dato a intercambiar}
+			seek(arch, filesize(arch)-1);                                             {voy al final del archivo}
+			read(arch, dato_reemplazo);
+			seek(arch, filesize(arch)-1);
+			write(arch, dato_baja);													{pongo el dato que voy a dar de baja en la ultima posicion}
+			seek(arch, filepos(arch)-1);
+			truncate(Arch);
+			seek(arch, pos);
+			write(arch, dato_reemplazo);	
+			seek(arch,filepos(arch)-1);                                             {vuelvo una posicion para volver a leer el registro que intercambie, en caso de que sea baja}
+		end;
+	end;
+	close(arch);
 end;
 
 {########################################################################}
@@ -88,9 +138,12 @@ VAR
 BEGIN
 	assign(archivo, 'data/maestro.dat');
 	assign(texto, 'data/maestro.txt');
-	generarMaestro(archivo);
+	generarMaestro(archivo, texto);
 	imprimirMaestro(archivo);
 	bajaLogica(Archivo);
+	imprimirMaestro(archivo);
+	compactarMaestro(archivo);
+	imprimirMaestro(archivo);
 	
 END.
 
